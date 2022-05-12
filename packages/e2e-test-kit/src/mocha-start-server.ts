9d5bef2028963663691660bf333df44c7327ac43
tzachbon <tzachbonfil@gmail.com>
2022-05-12 14:30:20 +0300

    infra: skip connecting to browser server when runnning only mocha current

--- packages/e2e-test-kit/src/mocha-start-server.ts
+++ packages/e2e-test-kit/src/mocha-start-server.ts
@@ -5,9 +5,11 @@ export const mochaGlobalSetup = async function (this: { server?: BrowserServer }
     await chromium.connectOverCDP(process.env.ENDPOINT_URL!).then((browser) => browser.close());
     console.log(`Chrome debugger instance found, trying to connect ${process.env.ENDPOINT_URL!}`);
   } catch (e) {
-    this.server = await chromium.launchServer();
-    process.env.PLAYWRIGHT_SERVER = this.server.wsEndpoint();
-    console.log(`Browser server running on ${process.env.PLAYWRIGHT_SERVER}`);
+    if (!process.env.ENDPOINT_URL) {
+      this.server = await chromium.launchServer();
+      process.env.PLAYWRIGHT_SERVER = this.server.wsEndpoint();
+      console.log(`Browser server running on ${process.env.PLAYWRIGHT_SERVER}`);
+    }
   }
 };
 
