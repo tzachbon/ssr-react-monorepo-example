fb3121b31c487d5051f04bc753c470e0cd3342f7
tzachbon <tzachbonfil@gmail.com>
2022-08-26 17:42:19 +0300

    chore: upgrade pleb jsdoc

--- pleb.config.mjs
+++ pleb.config.mjs
@@ -1,6 +1,8 @@
 // pleb.config.mjs
 
-/** @type import('pleb').Configuration */
+/**
+ * @type {import('pleb').PlebConfiguration}
+ */
 export default {
   pinnedPackages: [{ name: 'turbo', reason: 'Breaking change in minor version' }],
 };
